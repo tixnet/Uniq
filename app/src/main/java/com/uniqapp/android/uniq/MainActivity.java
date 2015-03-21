8e5968c3e69cad8b7c503fc537ec8aa21b521a3b
Mateusz Gazdziak <mateusz.gazdziak@gmail.com>
2015-03-21 01:38:03 +0100

    Setting transparent toolbar

--- app/src/main/java/com/uniqapp/android/uniq/MainActivity.java
+++ app/src/main/java/com/uniqapp/android/uniq/MainActivity.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends BaseActivity{
+
+public class MainActivity extends BaseActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO Implement fragments, set Geocoder
-
+        String city = "Krakow";
         Toolbar toolbar = getActionBarToolbar();
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
@@ -31,6 +40,18 @@ public class MainActivity extends BaseActivity{
 //        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng krakow = new LatLng(50.06465, 19.94498);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(krakow, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Kraków")
+                .snippet("Test")
+                .position(krakow));
     }
 
 
