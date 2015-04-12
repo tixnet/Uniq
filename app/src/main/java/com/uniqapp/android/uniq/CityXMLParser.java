3ae59d622cb5e432c82c2b8aaa58d26b263d2ce0
Mateusz Gazdziak <mateusz.gazdziak@gmail.com>
2015-04-12 16:51:10 +0200

    Slack test

---
+++ app/src/main/java/com/uniqapp/android/uniq/CityXMLParser.java
@@ -0,0 +1,31 @@
+package com.uniqapp.android.uniq;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Created by Mateusz Gaździak on 2015-04-12.
+ */
+public class CityXMLParser{
+
+    private static final String ns = null;
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+}
