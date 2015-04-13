a044aca8a470638dfc96061f279b19fe1a2ed7d0
Mateusz Gazdziak <mateusz.gazdziak@gmail.com>
2015-04-13 03:24:13 +0200

    Creating XML parser, working on AsyncTask

--- app/src/main/java/com/uniqapp/android/uniq/CityXMLParser.java
+++ app/src/main/java/com/uniqapp/android/uniq/CityXMLParser.java
@@ -7,6 +7,7 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,10 +23,118 @@ public class CityXMLParser{
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(in, null);
             parser.nextTag();
-            return readFeed(parser);
+            return readCity(parser);
         } finally {
             in.close();
         }
     }
+
+    private List readCity(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List zones = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "city");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("entry")) {
+                zones.add(readZone(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return zones;
+    }
+
+    public static class Zone {
+        public final String zoneName;
+        public final String coordinates;
+        public final String color;
+
+        private Zone(String zoneName, String coordinates, String color) {
+            this.zoneName = zoneName;
+            this.coordinates = coordinates;
+            this.color = color;
+        }
+    }
+
+    private Zone readZone(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "zone");
+        String zoneName = null;
+        String coordinates = null;
+        String color = null;
+        while (parser.next()!= XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("zoneName")) {
+                zoneName = readZoneName(parser);
+            }
+            else if (name.equals("coordinates")){
+                coordinates = readCoordinates(parser);
+            }
+            else if (name.equals("color")){
+                color = readColor(parser);
+            }
+            else skip(parser);
+
+
+        }
+
+        return new Zone(zoneName, coordinates, color);
+    }
+
+    private String readZoneName(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "zoneName");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "zoneName");
+        return title;
+    }
+
+    private String readCoordinates(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "coordinates");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "coordinates");
+        return title;
+    }
+
+    private String readColor(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "color");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "color");
+        return title;
+    }
+
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+
+
 
 }
--- app/src/main/java/com/uniqapp/android/uniq/MainActivity.java
+++ app/src/main/java/com/uniqapp/android/uniq/MainActivity.java
@@ -1,8 +1,10 @@
 package com.uniqapp.android.uniq;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -15,9 +17,19 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.melnykov.fab.FloatingActionButton;
+import com.uniqapp.android.uniq.CityXMLParser.Zone;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 
 public class MainActivity extends BaseActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +48,6 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
             public void onClick(View v) {
                 startService(new Intent(getApplicationContext(), ZoneCheckerService.class));
                 moveTaskToBack(true);
-
             }
         });
 
@@ -63,6 +74,7 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap map) {
         LatLng krakow = new LatLng(50.06465, 19.94498);
 
+
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(krakow, 13));
 
         map.addMarker(new MarkerOptions()
@@ -73,8 +85,57 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
         CircleOptions circleOptions = new CircleOptions()
                 .center(new LatLng(50.06465, 19.94498)).radius(1000).strokeWidth(1).fillColor(R.color.amber_A200);
 
-        Circle circle = map.addCircle(circleOptions);// In meters
+        Circle circle = map.addCircle(circleOptions);
+        // In meters
     }
 
+    private class OpenXmlTask extends AsyncTask<String,Void, String> {
+
+        @Override
+        protected String doInBackground(String... string) {
+            try {
+                //TODO
+                loadXml("test");
+                return "test";
+            }
+            catch (IOException e) {
+                Log.d(TAG, getResources().getString(R.string.io_error));
+                return getResources().getString(R.string.io_error);
+            }
+            catch (XmlPullParserException e) {
+                Log.d(TAG, getResources().getString(R.string.xml_error));
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+
+    private List loadXml(String city) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        CityXMLParser cityXMLParser = new CityXMLParser();
+        List<Zone> zones = null;
+        String zoneName = null;
+        String coordinates = null;
+        String color = null;
+
+        try {
+            stream = new FileInputStream("Krakow.xml");
+            zones = cityXMLParser.parse(stream);
+        }
+        catch (IOException e) {
+            Log.d(TAG, getResources().getString(R.string.io_error));
+        }
+        catch (XmlPullParserException e) {
+            Log.d(TAG, getResources().getString(R.string.xml_error));
+        }
+        finally {
+            if (stream != null) stream.close();
+        }
+        return zones;
+    }
 
 }
